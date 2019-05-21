76e3038501a710f28cfee4c4125c77cf4c801734
Luiz Ferraz <luiz.ferraz@starkbank.com>
2019-05-21 14:30:44 -0300

    Added missing indentation and instance check to Row middleware

--- febraban/cnab240/libs/middlewares/row.py
+++ febraban/cnab240/libs/middlewares/row.py
@@ -3,21 +3,21 @@ from ...characterType import alphaNumeric, numeric
 
 
 def validateFormatter(func):
-   def wrapper(self, string, charactersType, numberOfCharacters, defaultCharacter):
-       if type(numberOfCharacters) != int:
-           raise ValueError("numberOfCharacters must be Integer")
+    def wrapper(self, string, charactersType, numberOfCharacters, defaultCharacter):
+        if not isinstance(numberOfCharacters, int):
+            raise ValueError("numberOfCharacters must be Integer")
 
-       if charactersType not in [alphaNumeric, numeric]:
-           raise ValueError("charactersType must be alphaNumeric or numeric")
+        if charactersType not in (alphaNumeric, numeric):
+            raise ValueError("charactersType must be alphaNumeric or numeric")
 
-       if not isinstance(string, str):
-           raise ValueError("(%s,%s) is not an allowed value. It must be string" % (str(string), str(type(string))))
+        if not isinstance(string, str):
+            raise ValueError("(%s,%s) is not an allowed value. It must be string" % (str(string), str(type(string))))
 
-       regex = {
+        regex = {
            alphaNumeric: r'^[A-Za-z0-9\s\-]+$',
            numeric:      r'^[0-9]+$'
-       }[charactersType]
-       if string and not match(regex, string):
-           raise ValueError("You add %s that is a non %s value" % (string, charactersType))
-       return func(self, string, charactersType, numberOfCharacters, defaultCharacter)
-   return wrapper
+        }[charactersType]
+        if string and not match(regex, string):
+            raise ValueError("You add %s that is a non %s value" % (string, charactersType))
+        return func(self, string, charactersType, numberOfCharacters, defaultCharacter)
+    return wrapper
